8667849bbeb3840fa6f1bf96b7d6c379627df231
Sylvain <sylvain.barrellon@hotmail.fr>
2024-10-02 17:36:42 +0200

    mise en forme du contenu de la page d'accueil

--- view/HeaderView.php
+++ view/HeaderView.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?=isset($title)?$title:'Carosserie'?></title>
+    <title><?=isset($title)?$title:'Carrosserie'?></title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 
--- view/HomeView.php
+++ view/HomeView.php
@@ -1,1 +1,58 @@
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit consequuntur amet perspiciatis, veniam illo aperiam! Eius architecto recusandae expedita, explicabo placeat distinctio dolor quia repellat et quam, suscipit neque aspernatur?</p>
+<section class="banner">
+    <img src="assets/img/voitures-alignées.jpg" alt="plusieurs voitures alignées">
+</section>
+<section class="main-description">
+    <div class="main-div">
+        <div>
+            <h2>Carrosserie Barrellon</h2>
+            <p>Situé entre Saint-Étienne et Saint-Priest, notre garage automobile vous propose tous les travaux de carroserie. <br>
+                Que ce soit le débosselage, le passage au marbre, la peinture, la rénovation des optiques ou le changement de pare-brise, laissez-nous vous montrer notre savoir-faire pour la remise en état de votre véhicule. <br>
+                A l'écoute de vos besoin, nous réalisons tous les travax de carrosserie avec sérieux et professionnalisme en garantissant une qualité de travail irréprochable.
+            </p>
+        </div>
+        <img src="assets/img/phare-auto.jpg" alt="un phare arrière d'automobile">
+    </div>
+</section>
+<section class="main-adds">
+    <h3>Nos services</h3>
+    <div class="services">
+        <div class="service">
+            <h4>Devis gratuits</h4>
+        </div>
+        <div class="service">
+            <h4>Facilités de paiement</h4>
+            <p>Possibilités de paiement en plusieurs échéances</p>
+        </div>
+        <div class="service">
+            <h4>Certifications et agréments</h4>
+            <p>Agréé par plusieurs compagnies d'assurances</p>
+        </div>
+    </div>
+    <h3>Horaires</h3>
+    <div class="horaires">
+        <div class="horaire">
+            <h4>lundi</h4>
+            <p>8h/12h - 13h30/18h</p>
+        </div>
+        <div class="horaire">
+            <h4>mardi</h4>
+            <p>8h/12h - 13h30/18h</p>
+        </div>
+        <div class="horaire">
+            <h4>mercredi</h4>
+            <p>8h/12h - 13h30/18h</p>
+        </div>
+        <div class="horaire">
+            <h4>jeudi</h4>
+            <p>8h/12h - 13h30/18h</p>
+        </div>
+        <div class="horaire">
+            <h4>vendredi</h4>
+            <p>8h/12h - 13h30/18h</p>
+        </div>
+        <div class="horaire">
+            <h4>samedi</h4>
+            <p>8h/12h</p>
+        </div>
+    </div>
+</section>
