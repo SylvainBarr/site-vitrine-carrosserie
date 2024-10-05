096c13f76252b880174317cd1d8ab239d119242a
Sylvain <sylvain.barrellon@hotmail.fr>
2024-10-05 13:08:03 +0200

    modif des contactas du footer et ajout du createur du site dans le footer

--- view/FooterView.php
+++ view/FooterView.php
@@ -3,11 +3,12 @@
             <div class="contact">
                 <h4>Nous contacter</h4>
                 <div class="contacts">
-                    <p>28 Rue tartempion <br>42000 Saint-Étienne</p>
-                    <a href="mailto:contact@carroserie.com">contact@carroserie.com</a>
+                    <p><ion-icon name="home-outline"></ion-icon> 28 rue Tartempion <br>&nbsp; 42000 Saint-Étienne</p>
+                    <a href="mailto:contact@carroserie.com"><ion-icon name="at-outline"></ion-icon> contact@carroserie.com</a>
+                    <p><ion-icon name="call-outline"></ion-icon> 04 77 00 00 00</p>
                 </div>
             </div>
-            <p class="copy">Copyright ©2024 Carrosserie - <a href="?page=mentions-legales">Mentions légales</a></p>
+            <p class="copy">Copyright ©2024 Carrosserie - <a href="?page=mentions-legales">Mentions légales</a> - Réalisé par Sylvain Barrellon</p>
         </footer>
         <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
         <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
