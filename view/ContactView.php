eb1807262c1782c37d791884ae480170303f20de
Sylvain <sylvain.barrellon@hotmail.fr>
2024-10-12 17:53:21 +0200

    ajout de la map google en page de contact

---
+++ view/ContactView.php
@@ -0,0 +1,3 @@
+<section id="contact">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d349.75018533064383!2d4.379795886924514!3d45.469783363791976!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sfr!2sfr!4v1728748024139!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</section>
